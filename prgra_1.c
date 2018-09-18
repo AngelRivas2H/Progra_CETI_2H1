a238d02c57c614fff2bbe4331c60ff8cac960247
AngelRivas2H <43390466+AngelRivas2H@users.noreply.github.com>
2018-09-18 16:39:43 -0500

    Add files via upload

---
+++ prgra_1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+  int a = 8;
+  int b = 3;
+  int c = -5;
+  printf("OPERACIONES LOCAS DE PROGRA: \nBy: Angel Rivas \n \n");
+  printf("Grupo de ejercicios 1 \n \n");
+  printf("a. %d \n", a+b+c);
+  printf("b. %d \n", 2*b+3*(a-c));
+  printf("c. %d \n", a/b);
+  printf("d. %d \n", a%b);
+  printf("e. %d \n", a/c);
+  printf("f. %d \n", a%c);
+  printf("g. %d \n", a*b/c);
+  printf("h. %d \n", a*(b/c));
+  printf("i. %d \n", (a*b)%b);
+  printf("j. %d \n \n", a * ( c % b ));
+  printf("Grupo de ejercicios 2 \n \n");
+  float x = 88;
+  float y = 3.5;
+  float z = -5.2;
+  printf("a. %d \n", a+b+c);
+  printf("b. %d \n", 2*b+3*(a-c));
+  printf("c. %d \n", a/b);
+  printf("d. %d \n", a%b);
+  printf("e. %d \n", a/c);
+  printf("f. %d \n", a%c);
+  printf("g. %d \n", a*b/c);
+  printf("h. %d \n", a*(b/c));
+  printf("i. %d \n", (a*b)%b);
+  printf("j. %d \n \n", a * ( c % b ));
+}
